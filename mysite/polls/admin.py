2ca4b97cb0ecd4579c2c383076ebd9d93f1da014
Anthony <sean.a.protho1@gmail.com>
2019-11-15 20:44:49 -0800

    tutorial 6.5

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,9 +1,38 @@
-from django.contrib import admin
+#original
+# from django.contrib import admin
 
-#grabs from all models and imports our questions
+# #grabs from all models and imports our questions
+# from .models import Question
+
+# # Register your models here.
+
+# # #registers our models
+# # admin.site.register(Question)
+
+#customized admin page
+from django.contrib import admin
 from .models import Question
 
-# Register your models here.
+#makes publish date come before question field, may not be impressive on a small scale, but 
+# this is useful for large data organization
+# class QuestionAdmin(admin.ModelAdmin):
+#     fields = ['pub_date', 'question_text']
 
-#registers our models
-admin.site.register(Question)
+# admin.site.register(Question, QuestionAdmin)
+
+#this reorganizes the page, we add choice to .models and now make it admin available to create 
+# choice. With (admin.StackedInline) this creates our choices, but its too expanded, so we shorten
+# it with TabularInLine
+class ChoiceInline(admin.TabularInline):     
+    model = Choice
+    #num of choices
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register(Question, QuestionAdmin)
